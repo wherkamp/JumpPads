9a985b2d416ab449701f910ce082bb7d0fef645a
wherkamp <wherkamp@gmail.com>
2018-01-23 16:21:43 -0500

    Fixed a few bugs Ready for release :)

--- src/main/java/me/kingtux/kingjumppads/JumpListener.java
+++ src/main/java/me/kingtux/kingjumppads/JumpListener.java
@@ -1,6 +1,5 @@
 package me.kingtux.kingjumppads;
 
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -22,17 +21,15 @@ public class JumpListener implements Listener {
 		List<String> enabledWorlds = plugin.getConfig().getStringList("Enabled-Worlds");
 		if (enabledWorlds.contains("*") || enabledWorlds.contains(e.getPlayer().getLocation().getWorld().getName())) {
 			Player player = e.getPlayer();
-			String bottomBlock = plugin.getConfig().getString("BOTTOM_BLOCK").toUpperCase();
-			String topBlock = plugin.getConfig().getString("TOP_BLOCK").toUpperCase();
+			Material bottomBlock = Material.getMaterial(plugin.getConfig().getString("BOTTOM_BLOCK").toUpperCase());
+			Material topBlock = Material.getMaterial(plugin.getConfig().getString("TOP_BLOCK").toUpperCase());
 			Double angle = plugin.getConfig().getDouble("ANGLE");
-			String Sound = plugin.getConfig().getString("Sound");
-			Sound = Sound.toUpperCase();
-			if ((player.getLocation().subtract(0, 1, 0).getBlock().getType() == Material.getMaterial(bottomBlock))
-					&& (player.getLocation().getBlock().getType() == Material.getMaterial(bottomBlock))) {
+			String sound = plugin.getConfig().getString("Sound").toUpperCase();
+			if ((player.getLocation().subtract(0, 1, 0).getBlock().getType().equals(bottomBlock)
+					&& (player.getLocation().getBlock().getType().equals(topBlock)))) {
 				Vector v = player.getLocation().getDirection().multiply(10).setY(angle);
 				player.setVelocity(v);
-				player.playSound(player.getLocation(), String.valueOf(Sound), 5.0F, 2.0F);
-				player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("Jump-Message")));
+				player.playSound(player.getLocation(), sound, 5.0F, 2.0F);
 			}
 		}
 	}
