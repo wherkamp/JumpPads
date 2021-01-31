7fb3e86a7c90bb2e06ce66ea0fd47aff7dc89253
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2021-01-31 08:00:48 -0500

    Update

--- src/main/java/me/kingtux/kingjumppads/JumpPadsMain.java
+++ src/main/java/me/kingtux/kingjumppads/JumpPadsMain.java
@@ -1,15 +1,16 @@
 package me.kingtux.kingjumppads;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class JumpPadsMain extends JavaPlugin {
-	public void onEnable() {
-		getServer().getPluginManager().registerEvents(new JumpListener(this), this);
-		saveDefaultConfig();
-		BStats metrics = new BStats(this);
-	}
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new JumpListener(this), this);
+        saveDefaultConfig();
+        Metrics metrics = new Metrics(this, 2127);
+    }
 
-	public void onDisable(){
+    public void onDisable() {
 
-	}
+    }
 }
